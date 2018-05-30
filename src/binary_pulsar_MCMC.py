14d8d228f980e1be2f64caeee6342de1a1682145
Adam Deller <adeller@gmail.com>
2018-05-30 16:40:30 +1000

    Added GPL license

--- src/binary_pulsar_MCMC.py
+++ src/binary_pulsar_MCMC.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python
+
+#  This file is part of the astrometricfit distribution 
+#  (https://github.com/adamdeller/astrometricfit/).
+#  Copyright (c) 2018 Adam Deller and Scott Ransom
+#  
+#  This program is free software: you can redistribute it and/or modify  
+#  it under the terms of the GNU General Public License as published by  
+#  the Free Software Foundation, version 3.
+# 
+#  This program is distributed in the hope that it will be useful, but 
+#  WITHOUT ANY WARRANTY; without even the implied warranty of 
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+#  General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License 
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import numpy as np
 import astropy.coordinates as coords
 import astropy.units as u
